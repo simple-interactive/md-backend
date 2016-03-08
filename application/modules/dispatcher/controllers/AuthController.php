ca3002dcae053079adfff1e0a2fb0c588a9263cc
Sveridov Denis <sveridovdenis@ukr.net>
2016-03-08 16:02:50 +0200

    fix

--- application/modules/dispatcher/controllers/AuthController.php
+++ application/modules/dispatcher/controllers/AuthController.php
@@ -8,7 +8,10 @@ class Dispatcher_AuthController extends Zend_Controller_Action
         if ($config['dispatcher']['login'] === $this->getParam('login')
             && $config['dispatcher']['password'] === $this->getParam('password')){
             $this->view->success = true;
-            $this->view->token = $config['dispatcher']['token'];
+            $this->view->user = [
+                'token' => $config['dispatcher']['token'],
+                'email' => 'simple@simple.simple'
+            ];
         }
         else {
             $this->view->success = false;
